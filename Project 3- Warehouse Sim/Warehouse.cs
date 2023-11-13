61cf47c12a88e5d0d08eeda5d02dfa746ebccde2
SiriusSeptimus <tombo@LAPTOP-KLULIUKI>
2023-11-13 17:00:25 -0500

    IT FUCKING WORKS

--- Project 3- Warehouse Sim/Warehouse.cs
+++ Project 3- Warehouse Sim/Warehouse.cs
@@ -26,7 +26,7 @@ namespace Project_3__Warehouse_Sim
                                         // Takes 1 time increment to get a trunk out of the Entrance onto a Dock
         public Warehouse() 
         { 
-            for (int i = 0;  i < 10; i++)
+            for (int i = 1;  i <= 10; i++)
             {
                 Docks.Add(new Dock(i.ToString()));
             }
@@ -36,58 +36,84 @@ namespace Project_3__Warehouse_Sim
         {
             double chanceOfArrival;
             double chanceOfNoCrate;
-            Dock mostEmptyDock = new Dock("55");
-            Truck tempTruck = new Truck();
+            Dock mostEmptyDock = new Dock("-1");
+            Crate crateBeingHandled;                                                 //Creates a crate that represents the current crate being handled, to allow for storage of info
 
-            for(int i=1; i<=48; i++)
+
+            for (int i=1; i<=48; i++)
             {
-                Crate crateBeingHandled;                                                 //Creates a crate that represents the current crate being handled, to allow for storage of info
-             
-                for(int j=0; j < 10; j++)                                             //Loops through three times to determine if a truck arrives (between 1 and 3 per time increment)
+                Console.WriteLine($"\n\nNEW TIME INTERVAL {i}");
+
+                Console.WriteLine("\nTruck arrival time:");
+
+                for (int j=0; j < 3; j++)                                             //Loops through three times to determine if a truck arrives (between 1 and 3 per time increment)
                 {
+                    Truck tempTruck = new Truck();
+
                     chanceOfArrival = (rand.Next() % 100) / 100.00;                 //Generates a chance of arrival from 1 to 100, then divides that by 100 to get a percentage (ex. 0.38 = 38%) 
 
-                    if (i < 24 && chanceOfArrival <= (i / 24) + 0.1)                           //If the chance generated is less than the chance designated for the time frame (as per Gillenwater's suggestion)
+                    if (i < 24 && chanceOfArrival <= i / 24.00)                           //If the chance generated is less than the chance designated for the time frame (as per Gillenwater's suggestion)
                     {
-                        chanceOfNoCrate = (rand.Next() % 100) / 100.00;
+                        int crateNumber = 0;
                         do
                         {
-                            tempTruck.Load(new Crate());
-                        } while (tempTruck.Trailer.Count <= 12 && chanceOfArrival < 0.90);
+                            chanceOfNoCrate = (rand.Next() % 100) / 100.00;
+
+                            tempTruck.Load(new Crate(crateNumber.ToString()));
+                            crateNumber++;
+                        } while (tempTruck.Trailer.Count < 12 && chanceOfNoCrate < 0.90);
+
                         Entrance.Enqueue(tempTruck);                               //A new truck is enqueued into the entrance
-                        Console.WriteLine("Truck was enqueued!");
+                        Console.WriteLine($"\tTruck from {tempTruck.DeliveryCompany} was put in the entrance");
+                        
                     }
-                    else if (i >= 24 && chanceOfArrival <= ((48 - i) / 24) + 0.1)
+                    else if (i >= 24 && chanceOfArrival <= (48 - i) / 24.00)
                     {
-                        chanceOfNoCrate = (rand.Next() % 100) / 100.00;
-                        while (tempTruck.Trailer.Count <= 12 && tempTruck.Trailer.Count > 1 && chanceOfArrival < 0.90)
+                        int crateNumber = 0;
+                        do
                         {
-                            tempTruck.Load(new Crate());
-                        }
+                            chanceOfNoCrate = (rand.Next() % 100) / 100.00;
+
+                            tempTruck.Load(new Crate(crateNumber.ToString()));
+                            crateNumber++;
+                        } while (tempTruck.Trailer.Count < 12 && chanceOfNoCrate < 0.90);
+
                         Entrance.Enqueue(tempTruck);                               //A new truck is enqueued into the entrance
-                        Console.WriteLine("Truck was enqueued!");
+                        Console.WriteLine($"\tTruck from {tempTruck.DeliveryCompany} was put in the entrance");
                     }
                 }
-                  
-                if(Entrance.Count != 0)                                            //If the entrance has a truck in it (meaning it is not Null)
-                {
-                    foreach(Dock dock in Docks)                                     //The docks are searched
-                    {
-                        if(dock.TruckLine.Count() < mostEmptyDock.TruckLine.Count() || mostEmptyDock.Id == "55")
-                              mostEmptyDock = dock;                                   //And the least-filled dock is stored  
-                    }
 
-                    if(Entrance.Count != 0)
+                Console.WriteLine("\nTruck to dock transferral:");
+
+                for (int j = 0; j < 2; j++)
+                {
+                    if (Entrance.Count != 0)                                            //If the entrance has a truck in it (meaning it is not Null)
                     {
+
+                        Console.WriteLine($"\tEntrance has {Entrance.Count} trucks in it!");
+
+                        foreach (Dock dock in Docks)                                     //The docks are searched
+                        {
+                            if (dock.TruckLine.Count() < mostEmptyDock.TruckLine.Count() || mostEmptyDock.Id == "-1")
+                                mostEmptyDock = dock;                                   //And the least-filled dock is stored  
+                        }
+
+
+                        Console.WriteLine($"\tTruck from {Entrance.Peek().DeliveryCompany} was sent to dock {mostEmptyDock.Id} it has {Entrance.Peek().Trailer.Count} crates in it.");
+
                         mostEmptyDock.JoinLine(Entrance.Dequeue());                      //Once the most emtpy dock has been found, a truck is put into the doc's line.
-                        Console.WriteLine("Truck was Dequeued!");
-                    }
 
-                    
+                    }
                 }
-                   
-                foreach(Dock dock in Docks)                                         //Each dock is then allowed to do it's job in this time increment
+                
+                int dockNumber = 0;
+
+                Console.WriteLine("\nTruck unloading:");
+
+                foreach (Dock dock in Docks)                                         //Each dock is then allowed to do it's job in this time increment
                 {
+                    
+                    dockNumber++;
                     Truck temp = new Truck();
                     if(dock.TruckLine.Count != 0)
                     {
@@ -100,17 +126,19 @@ namespace Project_3__Warehouse_Sim
                     }
                     else if(temp.Trailer.Count == 0)           //This peeks into the dock's line (getting a Truck), then peeks into that truck's trailer to get a crate).
                     {                                                               //If the trailer has no more crates to unload, it will return "null"
-                       dock.SendOff();                                             //And the truck will be sent off on this time increment
-                       Console.WriteLine("Truck was emptied and sent off!");
+                        dock.SendOff();                                             //And the truck will be sent off on this time increment
+                        Console.WriteLine($"\tTruck from {temp.DeliveryCompany} finished unloading and was sent off of dock {dock.Id}");
                     }
                     else                                                            //If the dock has not needed to send off a Truck
                     {
-                       crateBeingHandled = dock.TruckLine.Peek().Unload();          //A crate is unloaded and stored in "crateBeingHandled"
-                       Console.WriteLine("Crate was unloaded");
+                        
+                        crateBeingHandled = temp.Unload();          //A crate is unloaded and stored in "crateBeingHandled"
+                        Console.WriteLine($"\t{temp.DeliveryCompany} on dock {dock.Id} was unloaded a crate. The truck now has {temp.Trailer.Count} crates left to unload.");
                           
                           //Store crate information here                              //The information of the crate is all stored and handled here.
                           //(unsure how this could work. Maybe Aurora's task?)
                     }
+                    
                 }                                                                   //The loop ends, the time increments, and the process is reapeated
             }       
           
